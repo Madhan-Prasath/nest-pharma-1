0fe58655a1fd0a71626bdc3a19c929696ff69e5b
madhanprasatha <madhanprasatha@bitsathy.ac.in>
2022-08-12 08:43:35 +0530

    StockistResource Model, Migration , Filament Resource and Policy Created

---
+++ app/Filament/Resources/StockistResource/Pages/CreateStockist.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\StockistResource\Pages;
+
+use App\Filament\Resources\StockistResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateStockist extends CreateRecord
+{
+    protected static string $resource = StockistResource::class;
+}
---
+++ app/Filament/Resources/StockistResource/Pages/ViewStockist.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\StockistResource\Pages;
+
+use App\Filament\Resources\StockistResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewStockist extends ViewRecord
+{
+    protected static string $resource = StockistResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+        ];
+    }
+}
